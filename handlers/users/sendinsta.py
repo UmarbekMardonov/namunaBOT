bdd9548fce0783b77b469950e5da71013cea17cf
Umarbek Mardonov <130063881+UmarbekMardonov@users.noreply.github.com>
2023-06-04 00:43:34 +0500

    q

--- handlers/users/sendinsta.py
+++ handlers/users/sendinsta.py
@@ -9,22 +9,23 @@ from insta import instadownloader
 @dp.message_handler(Text(startswith='https://www.instagram.com/'))
 async def send_media_insta(message: types.Message):
     link = message.text
+    link1 = '<a href="https://t.me/Yukla_video_1_bot/">Video Yukla Bot</a>'
     data = instadownloader(link=link)
     if data == 'Bad':
         await message.answer("Bu link orqali hech narsa topilmadi 😔 ")
     else:
         if data['type'] == 'video':
             await bot.send_video(chat_id=message.from_user.id, video=data['media'],
-                                 caption='<a href="https://www.t.me/Yukla_video_1_bot/">Video Yukla Bot</a>',
+                                 caption=link1,
                                  parse_mode='HTML')
         elif data['type'] == 'image':
             await bot.send_photo(chat_id=message.from_user, photo=data['media'],
-                                 caption='<a href="https://www.t.me/Yukla_video_1_bot/">Video Yukla Bot</a>',
+                                 caption=link1,
                                  parse_mode='HTML')
         elif data['type'] == 'carousel':
             for i in data['media']:
                 await bot.send_document(chat_id=message.from_user, document=data['media'],
-                                        caption='<a href="https://www.t.me/Yukla_video_1_bot/">Video Yukla Bot</a>',
+                                        caption=link1,
                                         parse_mode='HTML')
         else:
             await message.answer("Bu link orqali hech narsa topilmadi 😔 ")

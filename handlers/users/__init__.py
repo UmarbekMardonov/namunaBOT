19d43c96949b8edc9d5f7326e96318dee11c2bfb
Umarbek Mardonov <130063881+UmarbekMardonov@users.noreply.github.com>
2023-06-03 20:21:26 +0500

    changed

--- handlers/users/__init__.py
+++ handlers/users/__init__.py
@@ -1,5 +1,6 @@
 from . import help
 from . import start
+from . import menuKey
 from . import callbackhandlers
 from . import inlinemode
 from . import echo
--- handlers/users/start.py
+++ handlers/users/start.py
@@ -6,6 +6,5 @@ from loader import dp
 
 @dp.message_handler(CommandStart())
 async def bot_start(message: types.Message):
-    await message.answer(f"Salom, {message.from_user.full_name}!. Ushbu bot yordamida Instagram, YouTube va TikTok dan video yuklab olishingiz mumkin."
-                         f""
+    await message.answer(f"Assalomu Alaykum, {message.from_user.full_name}! Ushbu bot yordamida Instagram, YouTube va TikTok dan video yuklab olishingiz mumkin.\n"
                          f"/lang -> Botning tili || Bot language || Язык бота")

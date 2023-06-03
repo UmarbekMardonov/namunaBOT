62a82282bb9201bfef396c40330e4b1f0ab28b91
Umarbek Mardonov <130063881+UmarbekMardonov@users.noreply.github.com>
2023-06-03 20:27:32 +0500

    q

--- handlers/users/start.py
+++ handlers/users/start.py
@@ -9,4 +9,4 @@ async def bot_start(message: types.Message):
     await message.answer(f"Assalomu Alaykum, {message.from_user.full_name}! Ushbu bot yordamida Instagram, YouTube va TikTok dan video yuklab olishingiz mumkin.\n"
                          f"\n"
                          f"\n"
-                         f"/lang -> Botning tili || Bot language || Язык бота")
+                         f"/lang -> Bot tili || Bot language || Язык бота")

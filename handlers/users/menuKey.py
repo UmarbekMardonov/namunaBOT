2bb16a8d37b4d5244afe69384b81d382cc3be05c
Umarbek Mardonov <130063881+UmarbekMardonov@users.noreply.github.com>
2023-06-03 21:13:04 +0500

    q

--- handlers/users/menuKey.py
+++ handlers/users/menuKey.py
@@ -9,19 +9,19 @@ async def show_menu(message: Message):
     await message.answer("Tilni tanlang || Choose a language || Выберите язык", reply_markup=menu)
 
 
-@dp.message_handler(text="O'zbek")
+@dp.message_handler(text="O'zbek 🇺🇿")
 async def link_send(message: Message):
     await message.answer("Men ishlashim uchun menga Instagram, YouTube va TikTok dan "
                          "video link yuboring")
 
 
-@dp.message_handler(text="English")
+@dp.message_handler(text="English 🇬🇧")
 async def link_send2(message: Message):
     await message.answer("From Instagram, YouTube and TikTok send a video link"
                          "for me to work")
 
 
-@dp.message_handler(text="Русский")
+@dp.message_handler(text="Русский 🇷🇺")
 async def link_send3(message: Message):
     await message.answer("Из Instagram, YouTube и TikTok отправить ссылку на видео"
                          "мне на работу")

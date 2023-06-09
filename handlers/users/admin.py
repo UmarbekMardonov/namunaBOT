55bf360a3bbc4c6faea26033fb5ed0943e9854be
Umarbek Mardonov <130063881+UmarbekMardonov@users.noreply.github.com>
2023-06-10 04:32:56 +0500

    Update admin.py

--- handlers/users/admin.py
+++ handlers/users/admin.py
@@ -11,4 +11,12 @@ async def send_ad_to_all(message: types.Message):
     users = await db.select_all_users()
     for user in users:
         user_id = user[3]
-        await bot.send_message(chat_id=user_id, text=reklama)
+        if message.photo:
+            photo_file_id = message.photo[-1].file_id
+            photo_captain = message.caption or ""
+            await bot.send_photo(chat_id=user_id, photo=photo_file_id, caption=f"{reklama}\n{photo_captain}")
+        else:
+            await bot.send_message(chat_id=user_id, text=reklama)
+
+
+

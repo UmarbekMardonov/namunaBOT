aae30bf70f6a2033e7820b9457fcc6a1b13566f2
Umarbek Mardonov <130063881+UmarbekMardonov@users.noreply.github.com>
2023-06-10 03:56:35 +0500

    Update sendinsta.py

--- handlers/users/sendinsta.py
+++ handlers/users/sendinsta.py
@@ -8,7 +8,7 @@ from loader import bot, dp
 @dp.message_handler(Text(startswith='https://www.instagram.com/'))
 async def send_media_insta(message: types.Message):
     link = message.text
-    link1 = '<a href="https://t.me/Yukla_video_1_bot/">Video Yukla Bot</a> orqali yuklab olindi'
+    link1 = '🤖 <a href="https://t.me/Yukla_video_1_bot/">Video Yukla Bot</a> orqali yuklab olindi 📥'
     data = instadownloader(link=link)
     if data == 'Bad':
         await message.answer("Bu link orqali hech narsa topilmadi 😔 ")
@@ -21,15 +21,19 @@ async def send_media_insta(message: types.Message):
                                  parse_mode='HTML')
             await xxx.delete()
         elif data['type'] == 'image':
+            xxx = await message.answer(text='⌛️')
             await bot.send_chat_action(chat_id=message.from_user.id, action=ChatActions.UPLOAD_PHOTO)
             await bot.send_photo(chat_id=message.from_user.id, photo=data['media'],
                                  caption=link1,
                                  parse_mode='HTML')
+            await xxx.delete()
         elif data['type'] == 'carousel':
-            await bot.send_chat_action(chat_id=message.from_user.id, action=ChatActions.UPLOAD_VIDEO)
+            xxx = await message.answer(text='⌛️')
+            await bot.send_chat_action(chat_id=message.from_user.id, action=ChatActions.UPLOAD_PHOTO)
             for i in data['media']:
                 await bot.send_document(chat_id=message.from_user.id, document=data['media'],
                                         caption=link1,
                                         parse_mode='HTML')
+                await xxx.delete()
         else:
             await message.answer("Bu link orqali hech narsa topilmadi 😔 ")

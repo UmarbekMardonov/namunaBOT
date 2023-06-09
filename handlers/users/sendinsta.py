ee446225fac9df34f6e33775ec0e0bad97c11d53
Umarbek Mardonov <130063881+UmarbekMardonov@users.noreply.github.com>
2023-06-10 03:38:11 +0500

    Update sendinsta.py

--- handlers/users/sendinsta.py
+++ handlers/users/sendinsta.py
@@ -1,6 +1,6 @@
 from aiogram import types
 from aiogram.dispatcher.filters import Text
-from aiogram.types import ChatActions
+from aiogram.types import ChatActions, InputFile
 from insta import instadownloader
 from loader import bot, dp
 
@@ -8,21 +8,25 @@ from loader import bot, dp
 @dp.message_handler(Text(startswith='https://www.instagram.com/'))
 async def send_media_insta(message: types.Message):
     link = message.text
+    uploading = InputFile('uploading_animation.gif')
     link1 = '<a href="https://t.me/Yukla_video_1_bot/">Video Yukla Bot</a> orqali yuklab olindi'
     data = instadownloader(link=link)
     if data == 'Bad':
         await message.answer("Bu link orqali hech narsa topilmadi 😔 ")
     else:
         if data['type'] == 'video':
+            await message.answer_animation(uploading)
             await bot.send_chat_action(chat_id=message.from_user.id, action=ChatActions.UPLOAD_VIDEO)
             await bot.send_video(chat_id=message.from_user.id, video=data['media'],
                                  caption=link1,
                                  parse_mode='HTML')
         elif data['type'] == 'image':
+            await bot.send_chat_action(chat_id=message.from_user.id, action=ChatActions.UPLOAD_PHOTO)
             await bot.send_photo(chat_id=message.from_user.id, photo=data['media'],
                                  caption=link1,
                                  parse_mode='HTML')
         elif data['type'] == 'carousel':
+            await bot.send_chat_action(chat_id=message.from_user.id, action=ChatActions.UPLOAD_VIDEO)
             for i in data['media']:
                 await bot.send_document(chat_id=message.from_user.id, document=data['media'],
                                         caption=link1,

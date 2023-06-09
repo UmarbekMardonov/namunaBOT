8538f3ff820fd5bd8c4aa37be7c5b2a81a700af9
Umarbek Mardonov <130063881+UmarbekMardonov@users.noreply.github.com>
2023-06-10 04:48:59 +0500

    Update admin.py

--- handlers/users/admin.py
+++ handlers/users/admin.py
@@ -5,13 +5,12 @@ from data.config import ADMINS
 from loader import dp, db, bot
 
 
-@dp.message_handler(Text(startswith="#message"), user_id=ADMINS)
+@dp.message_handler(text="/message", user_id=ADMINS)
 async def send_ad_to_all(message: types.Message):
-    reklama = message.text
     users = await db.select_all_users()
     for user in users:
         user_id = user[3]
-        await bot.send_message(chat_id=user_id, text=reklama)
+        await bot.send_message(chat_id=user_id, text="...")
 
 
 

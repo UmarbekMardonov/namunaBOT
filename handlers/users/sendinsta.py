149420f2051002029a7d262396ec4db25537e9ca
Umarbek Mardonov <130063881+UmarbekMardonov@users.noreply.github.com>
2023-06-10 03:48:47 +0500

    Update sendinsta.py

--- handlers/users/sendinsta.py
+++ handlers/users/sendinsta.py
@@ -8,18 +8,18 @@ from loader import bot, dp
 @dp.message_handler(Text(startswith='https://www.instagram.com/'))
 async def send_media_insta(message: types.Message):
     link = message.text
-    uploading = InputFile('uploading_animation.gif')
     link1 = '<a href="https://t.me/Yukla_video_1_bot/">Video Yukla Bot</a> orqali yuklab olindi'
     data = instadownloader(link=link)
     if data == 'Bad':
         await message.answer("Bu link orqali hech narsa topilmadi 😔 ")
     else:
         if data['type'] == 'video':
-            await message.answer_animation(uploading)
+            xxx = await message.answer(text='⌛️')
             await bot.send_chat_action(chat_id=message.from_user.id, action=ChatActions.UPLOAD_VIDEO)
             await bot.send_video(chat_id=message.from_user.id, video=data['media'],
                                  caption=link1,
                                  parse_mode='HTML')
+            await xxx.delete()
         elif data['type'] == 'image':
             await bot.send_chat_action(chat_id=message.from_user.id, action=ChatActions.UPLOAD_PHOTO)
             await bot.send_photo(chat_id=message.from_user.id, photo=data['media'],

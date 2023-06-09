7b4f8b8d7bea333e1d63f08fafeed8ee44cc4a72
Umarbek Mardonov <130063881+UmarbekMardonov@users.noreply.github.com>
2023-06-10 04:17:51 +0500

    Update admin.py

--- handlers/users/admin.py
+++ handlers/users/admin.py
@@ -1,11 +1,14 @@
 from aiogram import types
+from aiogram.dispatcher.filters import Text
+
 from data.config import ADMINS
 from loader import dp, db, bot
 
 
-@dp.message_handler(text="/reklama", user_id=ADMINS)
+@dp.message_handler(Text(startswith="#reklama"), user_id=ADMINS)
 async def send_ad_to_all(message: types.Message):
+    reklama = message.text
     users = await db.select_all_users()
     for user in users:
         user_id = user[3]
-        await bot.send_message(chat_id=user_id, text="Reklama")
+        await bot.send_message(chat_id=user_id, text=reklama)

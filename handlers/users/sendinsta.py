b75a4f6965ae6c9f1a7ee3e1a38f54f4fcdf7e48
Umarbek Mardonov <130063881+UmarbekMardonov@users.noreply.github.com>
2023-06-10 20:40:18 +0500

    Update sendinsta.py

--- handlers/users/sendinsta.py
+++ handlers/users/sendinsta.py
@@ -1,3 +1,4 @@
+import aiogram.utils.exceptions
 from aiogram import types
 from aiogram.dispatcher.filters import Text
 from aiogram.types import ChatActions, InputFile
@@ -11,28 +12,32 @@ async def send_media_insta(message: types.Message):
     xxx = await message.answer(text='⌛️')
     link1 = '🤖 <a href="https://t.me/Yukla_video_1_bot/">Video Yukla Bot</a> orqali yuklab olindi 📥'
     data = instadownloader(link=link)
-    if data == 'Bad':
-        await message.answer("Bu link orqali hech narsa topilmadi 😔 ")
-    else:
-        if data['type'] == 'video':
-            await bot.send_chat_action(chat_id=message.from_user.id, action=ChatActions.UPLOAD_VIDEO)
-            await bot.send_video(chat_id=message.from_user.id, video=data['media'],
-                                 caption=link1,
-                                 parse_mode='HTML')
-            await xxx.delete()
-        elif data['type'] == 'image':
-            await bot.send_chat_action(chat_id=message.from_user.id, action=ChatActions.UPLOAD_PHOTO)
-            await bot.send_photo(chat_id=message.from_user.id, photo=data['media'],
-                                 caption=link1,
-                                 parse_mode='HTML')
-            await xxx.delete()
-        elif data['type'] == 'carousel':
-            await bot.send_chat_action(chat_id=message.from_user.id, action=ChatActions.UPLOAD_PHOTO)
-            for i in data['media']:
-                await bot.send_document(chat_id=message.from_user.id, document=i,
-                                        caption=link1,
-                                        parse_mode='HTML')
-        else:
-            await message.answer("Bu link orqali hech narsa topilmadi 😔 ")
-    await xxx.delete()
+    try:
 
+        if data == 'Bad':
+            await message.answer("Bu link orqali hech narsa topilmadi 😔 ")
+        else:
+            if data['type'] == 'video':
+                await bot.send_chat_action(chat_id=message.from_user.id, action=ChatActions.UPLOAD_VIDEO)
+                await bot.send_video(chat_id=message.from_user.id, video=data['media'],
+                                     caption=link1,
+                                     parse_mode='HTML')
+                await xxx.delete()
+            elif data['type'] == 'image':
+                await bot.send_chat_action(chat_id=message.from_user.id, action=ChatActions.UPLOAD_PHOTO)
+                await bot.send_photo(chat_id=message.from_user.id, photo=data['media'],
+                                     caption=link1,
+                                     parse_mode='HTML')
+                await xxx.delete()
+            elif data['type'] == 'carousel':
+                await bot.send_chat_action(chat_id=message.from_user.id, action=ChatActions.UPLOAD_PHOTO)
+                for i in data['media']:
+                    await bot.send_document(chat_id=message.from_user.id, document=i,
+                                            caption=link1,
+                                            parse_mode='HTML')
+            else:
+                await message.answer("Bu link orqali hech narsa topilmadi 😔 ")
+        await xxx.delete()
+    except aiogram.utils.exceptions.InvalidHTTPUrlContent:
+        await message.answer("Bu linkda hech qanday post topilmadi😞\n"
+                             "Iltimos boshqa link yuboring👇")

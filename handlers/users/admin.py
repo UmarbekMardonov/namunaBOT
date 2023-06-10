64b3a2f3221426352fd66d2cbdf7779934d55b33
Umarbek Mardonov <130063881+UmarbekMardonov@users.noreply.github.com>
2023-06-10 20:50:10 +0500

    Update admin.py

--- handlers/users/admin.py
+++ handlers/users/admin.py
@@ -10,7 +10,7 @@ async def send_ad_to_all(message: types.Message):
     users = await db.select_all_users()
     for user in users:
         user_id = user[3]
-        await bot.send_message(chat_id=user_id, text="...")
+        await bot.send_message(chat_id=user_id, text="Botning kamchiliklari to'g'rlandi😊")
 
 
 
